e2948187fdb87cd4428636cf5502e785fa2c1208
Sanaz Behbahani <sanaz.behbahani@gmail.com>
2019-06-06 18:19:31 +0000

    define BFS expansion list

--- pathplanner.cpp
+++ pathplanner.cpp
@@ -73,6 +73,79 @@ void print2DVector (T Vec)
 	cout << endl;
     };
 };
+
+// Search function which generates the expansions
+void search (Map map, Planner planner)
+{
+    // Create a closed 2D array filled with 0s and 1 as its first element
+    vector <vector <int>> closed (map.mapHeight, vector <int> (map.mapWidth));
+    closed [planner.start[0]][planner.start[1]];
+    
+    // Define the triplet
+    int x = planner.start[0];
+    int y = planner.start[1];
+    int g = 0;
+
+    // Store the expansion
+    vector <vector <int>> open;
+    open.pushback ({g, x, y});
+    
+    // Flags
+    bool found = false;
+    bool resign = false;
+
+    int x2;
+    int y2;
+    
+    // While I am still searching for the goal and problem is solvable
+    while (!found && !resign){
+	// Resign if no value in the open list and you can't expand anymore
+	if (open.size == 0){
+	    resign = true;
+	    cout << "Failed to reach goal :(" << endl;
+	};
+	// Keep expanding
+	else {
+	    // Remove triplets from open list
+	    sort (open.begin(), open.end());
+	    reverse (open.begin(), open.end());
+	    vector <int> next; 
+	    // Store poped value in next
+	    next = open.back();
+	    open.pop_back();
+
+	    g = next [0];
+	    x = next [1];
+	    y = next [2];
+
+	    // Check if we reached the goal
+    	    if (x == planner.goal[0] && y == planner.goal[1]){
+		found = true;
+		cout << "[" << g << ", " << x << ", " << y << "]" << endl;
+	    };
+
+   	    // else expand new element
+            else{
+		for (int i = 0; i < planner.movements.size(); i++){
+		    x2 = x + planner.movements [i][0];
+		    y2 = y + planner.movements [i][1];
+		    if (x2 >= 0 && x2 < map.grid.size() && y2 >= 0 && y2 < map.grid[0].size()){
+			if (closed [x2][y2] == 0 && map.grid [x2][y2] == 0){
+			    int g2 = g + planner.cost;
+			    open.push_back ({g2, x2, y2});
+			    closed [x2][y2] = 1; 
+			};
+		    };
+		};
+
+	    };
+    	    
+	};
+
+    };
+
+    
+};
     
 
 /*############ Don't modify the main function############*/
@@ -91,6 +164,9 @@ int main()
     cout << "Robot Movements: " << planner.movements_arrows[0] << " , " << planner.movements_arrows[1] << " , " << planner.movements_arrows[2] << " , " << planner.movements_arrows[3] << endl;
     cout << "Delta:" << endl;
     print2DVector(planner.movements);
+
+    // Search for the expansions
+    search (map, planner);
 
     return 0;
 }
